868c0358f60f398395f2af90de30784dc59b461b
André Simões <abaioasimoes@apple.com>
2016-01-26 21:04:14 +0000

    Initial Commit

---
+++ Thermostat/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Thermostat
+//
+//  Created by André Simões on 26/01/16.
+//  Copyright © 2016 Andre Simoes. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
