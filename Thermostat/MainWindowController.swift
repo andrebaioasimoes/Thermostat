dc3aad23dc89caff83115b6b208ac75287fa2553
André Simões <abaioasimoes@apple.com>
2016-01-26 22:10:39 +0000

    first commit

---
+++ Thermostat/MainWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  MainWindowController.swift
+//  Thermostat
+//
+//  Created by André Simões on 26/01/16.
+//  Copyright © 2016 Andre Simoes. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
